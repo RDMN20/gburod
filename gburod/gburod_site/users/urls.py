e078f8dd8bbb7544d04d93da0362ff7eadcdac2b
RDMN20 <129740249+RDMN20@users.noreply.github.com>
2023-04-18 12:32:37 +0300

    Develop (#24)
    * Add structure content
    
    * Update structure page
    
    * Update settings.py
    
    * Fix depart_detail page
    
    * Fix depart_detail page (#14)
    
    Co-authored-by: ramzandashkar <dashkarov@gmail.com>
    
    * Update .env
    
    * Update carousel slide
    
    * Update css
    
    * Update structure page
    
    * Feature/content (#16)
    
    * Fix depart_detail page
    
    * Update carousel slide
    
    * Update css
    
    * Update structure page
    
    ---------
    
    Co-authored-by: ramzandashkar <dashkarov@gmail.com>
    
    * Fix paginator
    
    * Fix .env
    
    * Add rate on structure page
    
    * Feature/content (#19)
    
    * Fix depart_detail page
    
    * Update carousel slide
    
    * Update css
    
    * Update structure page
    
    * Add rate on structure page
    
    ---------
    
    Co-authored-by: ramzandashkar <dashkarov@gmail.com>
    
    * Feature/content (#23)
    
    * Update css
    
    * Add redirect for users
    
    ---------
    
    Co-authored-by: ramzandashkar <dashkarov@gmail.com>
    
    ---------
    
    Co-authored-by: ramzandashkar <dashkarov@gmail.com>

--- gburod/gburod_site/users/urls.py
+++ gburod/gburod_site/users/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     ),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'),
+        views.Login.as_view(template_name='users/login.html'),
         name='login'
     ),
     path(
