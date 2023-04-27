38931e9d4b9c0d2d15f4d150a668140a7aa7248d
ramzandashkar <dashkarov@gmail.com>
2023-04-27 16:59:51 +0300

    Add licenses

--- gburod/gburod_site/structure/models.py
+++ gburod/gburod_site/structure/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 
-
+class License(models.Model):
+    title = models.CharField(
+        max_length=250,
+        verbose_name='Заголовок лицензии'
+    )
+    type = models.CharField(
+        max_length=250,
+        verbose_name='Тип лицензии'
+    )
+    file = models.FileField(
+        upload_to='licenses/',
+        max_length=100,
+        verbose_name='Файл лицензии',
+        help_text='Загрузите файл лицензии в формате PDF'
+    )
