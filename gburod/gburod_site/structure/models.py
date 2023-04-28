b7c569b47e102f2b6827f10b2ddf65a0d383d259
ramzandashkar <dashkarov@gmail.com>
2023-04-28 16:49:50 +0300

    Add documents page

--- gburod/gburod_site/structure/models.py
+++ gburod/gburod_site/structure/models.py
@@ -10,9 +10,20 @@ class License(models.Model):
         max_length=250,
         verbose_name='Тип лицензии'
     )
+    licenses_num = models.CharField(
+        max_length=250,
+        verbose_name='Номер лицензии'
+    )
     file = models.FileField(
         upload_to='licenses/',
         max_length=100,
         verbose_name='Файл лицензии',
         help_text='Загрузите файл лицензии в формате PDF'
     )
+
+    class Meta:
+        verbose_name = 'Лицензия'
+        verbose_name_plural = 'Лицензии'
+
+    def __str__(self):
+        return f'{self.title}'
