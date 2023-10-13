b3ade76bbf3bd20e31a139a70774e35478164fa1
ramzandashkar <dashkarov@gmail.com>
2023-10-13 14:02:53 +0300

    Update template and docker

--- gburod/gburod_site/gburod_site/urls.py
+++ gburod/gburod_site/gburod_site/urls.py
@@ -17,14 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from django.views.static import serve
 
 
-
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('main_page.urls')),
     path('structure/', include('structure.urls')),
--- gburod/gburod_site/structure/urls.py
+++ gburod/gburod_site/structure/urls.py
@@ -7,9 +7,13 @@ app_name = 'structure'
 urlpatterns = [
     path('', views.structures, name='structures'),
     path('ambulant/', views.ambulant, name='ambulant'),
-    path('depart_detail/<int:department_id>/', views.depart_detail, name='depart_detail'),
-    path('persona_detail/<int:persona_id>/', check_recaptcha(views.persona_detail), name='persona_detail'),
-    path('persona_detail/qr/<int:persona_code>/', check_recaptcha(views.persona_detail), name='persona_qr_detail'),
-    path('persona_detail/<int:persona_id>/comment/', views.add_comment, name='add_comment'),
+    path('depart_detail/<int:department_id>/', views.depart_detail,
+         name='depart_detail'),
+    path('persona_detail/<int:persona_id>/',
+         check_recaptcha(views.persona_detail), name='persona_detail'),
+    path('persona_detail/qr/<int:persona_code>/',
+         check_recaptcha(views.persona_detail), name='persona_qr_detail'),
+    path('persona_detail/<int:persona_id>/comment/', views.add_comment,
+         name='add_comment'),
     path('licenses/', views.licenses, name='licenses'),
 ]
