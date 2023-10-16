ad0ab7fb3e4345deaa47af07f29a7774ac4e9dc5
ramzandashkar <dashkarov@gmail.com>
2023-10-16 15:54:00 +0300

    Update url, docker-compose

--- gburod/gburod_site/gburod_site/urls.py
+++ gburod/gburod_site/gburod_site/urls.py
@@ -19,10 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 
-
 urlpatterns = [
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # path(r'^media/(?P<path>.*)$', serve,
+    #      {'document_root': settings.MEDIA_ROOT}),
+    # path(r'^static/(?P<path>.*)$', serve,
+    #      {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('main_page.urls')),
     path('structure/', include('structure.urls')),
@@ -31,7 +32,7 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+#     )
