95eae89c6a0e0cc5aab99eef0b7876bbe36cc032
ramzandashkar <dashkarov@gmail.com>
2023-10-31 12:03:05 +0300

    Fixed PEP8

--- gburod/gburod_site/main_page/views.py
+++ gburod/gburod_site/main_page/views.py
@@ -5,6 +5,7 @@ def index(request):
     template = 'main_page/index.html'
     return render(request, template)
 
+
 def about_us(request):
     template = 'main_page/about.html'
     return render(request, template)
