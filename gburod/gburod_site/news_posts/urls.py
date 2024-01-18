427e89f684091f5f1199bedc7fa7aa754c0155b0
ramzandashkar <dashkarov@gmail.com>
2024-01-18 14:14:28 +0300

    Update news. Add news detail on main page and news page.

--- gburod/gburod_site/news_posts/urls.py
+++ gburod/gburod_site/news_posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from news_posts.views import news_list, news_detail
 
 
 app_name = 'news-posts'
 urlpatterns = [
-    path('', views.news_list, name='news'),
+    path('', news_list, name='news'),
+    path('news-detail/<int:news_id>/', news_detail, name='news_detail'),
 ]
